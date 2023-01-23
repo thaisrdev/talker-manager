b59ebbfd6174c6b02fd6f88552477dc7566b9b84
Thais Rosa <tsrosa18@gmail.com>
2023-01-22 22:10:50 -0300

    Fix: Requisito 1

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
+app.get('/talker', async (req, res) => {
+  const promise = await fs.readFile('src/talker.json', 'utf-8');
+  const data = JSON.parse(promise);
+  res.status(HTTP_OK_STATUS).json(data);
+});
+
 app.get('/talker/:id', async (req, res) => {
   const { id } = req.params;
   const promise = await fs.readFile('src/talker.json', 'utf-8');
